711a6d4be2da1a0da25a9884ddfe4b36de84b6ee
lasya2206 <94067773+lasya2206@users.noreply.github.com>
2022-05-06 22:19:03 +0530

    first commit

---
+++ contact form/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // Theme
+    theme: "lightBlue",
+
+    // Email notifier account (sender)
+    host: "smtp.gmail.com", // Sender email smtp
+    port: "465", // Sender email port
+    user: "lasyagadiyaram@gmail.com", // Sender email username
+    pass: "fpnpxemxkysoromp", // Sender email password
+
+    // Your email to receive notification (receiver)  
+    from: '"Contact Me" <noreply@example.com>', // Sender email address
+    to: 'lasyagadiyaram@gmail.com', // Your email address
+    subject: 'Contact Us', // Subject
+};
